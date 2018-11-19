dc63c2220fbdb1281fee6458296b1a4ac9526c97
azrael8576 <azrael8576love2008@gmail.com>
2018-11-19 19:07:17 +0800

    8. Create the Repository

---
+++ app/src/main/java/com/alex/roomwordsample/WordRepository.java
@@ -0,0 +1,36 @@
+package com.alex.roomwordsample;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class WordRepository {
+
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+    WordRepository(Application application){
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAllWords();
+    }
+    LiveData<List<Word>> getmAllWords(){
+        return mAllWords;
+    }
+    public void insert (Word word) {
+        new insertAsyncTask(mWordDao).execute(word);
+    }
+    
+    private static class insertAsyncTask extends AsyncTask<Word,Void,Void>{
+        private WordDao mAsyncTaskDao;
+        insertAsyncTask(WordDao dao){
+            mAsyncTaskDao = dao;
+        }
+        @Override
+        protected Void doInBackground(Word... words) {
+            return null;
+        }
+    }
+}
