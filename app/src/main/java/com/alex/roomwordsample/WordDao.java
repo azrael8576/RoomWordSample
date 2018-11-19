f37ae0ffa50aa1141fc9ab217b97a4ced9a7ae15
azrael8576 <azrael8576love2008@gmail.com>
2018-11-19 17:29:27 +0800

    Create the DAO

---
+++ app/src/main/java/com/alex/roomwordsample/WordDao.java
@@ -0,0 +1,17 @@
+package com.alex.roomwordsample;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+    @Insert
+    void insert(Word word);
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+    @Query("SELECT * FROM word_table BY word ASC")
+    List<Word> getAllWords();
+}
